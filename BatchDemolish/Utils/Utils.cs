d576112af662ef2dd23ff0389ec42a996c969e48
Tom E Flaherty <teflaherty@utexas.edu>
2023-07-20 16:55:21 -0500

    still 1 error

--- BatchDemolish/Utils/Utils.cs
+++ BatchDemolish/Utils/Utils.cs
@@ -31,18 +31,71 @@ namespace BatchDemolish
                     Element curElem = curDoc.GetElement(pick);
 
                     ElementId curElemId = curElem.Id;
-                    
-                    //ElementId selectedPhaseID = curWin.selectedPhase
+
+                    Phase selectedDemoPhase = Utils.getPhaseByName(curDoc, curWin.selectedPhase);
+
+                    ElementId selectedPhaseId = selectedDemoPhase.Id;
 
                     Parameter paramPhaseDemo = curElem.get_Parameter(BuiltInParameter.PHASE_DEMOLISHED);
 
                     if (paramPhaseDemo != null)
                     {
-                        paramPhaseDemo.Set(curWin.selectedPhase);
+                        paramPhaseDemo.Set(ElementId.selectedPhaseId);
                     }
                 }               
             }
         }
+
+        #region Phases
+
+        public static List<Element> getAllPhases(Document curDoc)
+        {
+            FilteredElementCollector curColl = new FilteredElementCollector(curDoc);
+            curColl.OfCategory(BuiltInCategory.OST_Phases);
+
+            return curColl.OrderBy(x => x.Name).ToList();
+        }
+
+        public static List<Element> getAllPhaseFilters(Document curDoc)
+        {
+            FilteredElementCollector curColl = new FilteredElementCollector(curDoc);
+            curColl.OfClass(typeof(PhaseFilter));
+
+            return curColl.OrderBy(x => x.Name).ToList();
+        }
+
+        public static Phase getPhaseByName(Document curDoc, string phaseName)
+        {
+            // get all phases
+            List<Element> phaseList = getAllPhases(curDoc);
+
+            foreach (Phase curPhase in phaseList)
+            {
+                if (curPhase.Name == phaseName)
+                    return curPhase as Phase;
+            }
+
+            return null;
+
+        }
+
+        public static PhaseFilter getPhaseFilterByName(Document curDoc, string phaseFilterName)
+        {
+            // get all phase filters
+            List<Element> phaseFilterList = getAllPhaseFilters(curDoc);
+
+            foreach (PhaseFilter curPhaseFilter in phaseFilterList)
+            {
+                if (curPhaseFilter.Name == phaseFilterName)
+                {
+                    return curPhaseFilter as PhaseFilter;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
 
         #region Ribbon
 
